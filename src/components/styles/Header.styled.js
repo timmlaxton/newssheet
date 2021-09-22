e01c5a8496710745b1c84154e85aa8ebcabbd5f9
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-09-22 19:36:45 +0100

    added Header, Container and Global styled components

---
+++ src/components/styles/Header.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  background-color: ${({ theme }) => theme.colors.header};
+  padding: 40px 0%;
+
+  h1 {
+    color: black;
+  }
+`;
