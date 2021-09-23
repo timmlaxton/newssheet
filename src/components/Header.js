6eb2f3ad66e8c536ee238744d6a2e7ea40aa1c7e
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-09-23 20:06:24 +0100

    added styed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import { StyledHeader } from "./styles/Header.styled";
+import { Nav } from "./styles/Header.styled";
+import { Logo } from "./styles/Header.styled";
 import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
 
 export default function Header() {
   return (
     <StyledHeader>
       <Container>
-        <h1>Hubble</h1>
+        <Nav>
+          <Logo src="./images/logo.svg" alt="" />
+          <Button>Try ti for free</Button>
+        </Nav>
       </Container>
     </StyledHeader>
   );
